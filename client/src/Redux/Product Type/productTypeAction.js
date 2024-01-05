42716f282c8620622ab3e43cf917e891b3dbf524
madukaHasantha <77095852+madukaHasantha@users.noreply.github.com>
2024-01-05 20:49:32 +0530

    fet: changes some printing messages in supplier action, product name action and product type action

--- client/src/Redux/Product Type/productTypeAction.js
+++ client/src/Redux/Product Type/productTypeAction.js
@@ -1,7 +1,7 @@
 import { PRODUCT_TYPE_GET_LIST} from "../constatnt"
 
 export const productTypeGetList = () => {
-    console.log("productTypeGetList action is called");
+    console.log("productTypeGetList action is calledd");
     return{
         type: PRODUCT_TYPE_GET_LIST
     }
--- client/src/Redux/Supplier/supplierAction.js
+++ client/src/Redux/Supplier/supplierAction.js
@@ -1,7 +1,7 @@
 import { SUPPLIER_GET_LIST} from "../constatnt"
 
 export const supplierGetList = () => {
-    console.log("supplierGetList action is called");
+    console.log("supplierGetList action is calledd");
     return{
         type: SUPPLIER_GET_LIST
     }
