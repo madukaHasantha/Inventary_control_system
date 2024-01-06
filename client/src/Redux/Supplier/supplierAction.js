14b53ea1e14e35011abb0849ebadb9f0580a4d62
madukaHasantha <77095852+madukaHasantha@users.noreply.github.com>
2024-01-06 21:28:21 +0530

    do some changes in redux

--- client/src/Redux/Supplier/supplierAction.js
+++ client/src/Redux/Supplier/supplierAction.js
@@ -1,7 +1,7 @@
 import { SUPPLIER_GET_LIST} from "../constatnt"
 
 export const supplierGetList = () => {
-    console.log("supplierGetList action is calledd");
+    console.log("supplierGetList action is called");
     return{
         type: SUPPLIER_GET_LIST
     }
