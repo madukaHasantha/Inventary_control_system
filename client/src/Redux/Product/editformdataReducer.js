583d47b247db04c4a15e0d4af050f2293c2c8401
madukaHasantha <77095852+madukaHasantha@users.noreply.github.com>
2024-01-14 02:20:04 +0530

    dev: fix issue in update form frontend divided to component

--- client/src/Redux/Product/editformdataReducer.js
+++ client/src/Redux/Product/editformdataReducer.js
@@ -1,16 +1,11 @@
-import { SET_PRODUCT_LIST } from "../constatnt";
-
-const initialState = {
-    emptyData: []
-};
+import { EDIT_FORM_DATA } from "../constatnt";
 
 export const editFormReducerData = (state = [], action) => {
-  console.log("Edit form reducer data is called", action.payload);
+  console.log("Edit form reducer data is called", action.payload, typeof(action.payload));
 
   switch (action.type) {
-    case SET_PRODUCT_LIST:
-      return [...action.payload];
-      
+    case EDIT_FORM_DATA:
+      return typeof action.payload === 'object' ? { ...action.payload } : state;
     default:
       return state;
   }
