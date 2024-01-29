a0b1b638247af4cab559b46dd6fa32ccf4fffac3
madukaHasantha <77095852+madukaHasantha@users.noreply.github.com>
2024-01-29 17:39:33 +0530

    dev: some changes in frontend

--- client/src/Redux/Product Type/productTypeAction.js
+++ client/src/Redux/Product Type/productTypeAction.js
@@ -1,7 +1,7 @@
 import { PRODUCT_TYPE_GET_LIST} from "../constatnt"
 
 export const productTypeGetList = () => {
-    console.log("productTypeGetList action is calledd");
+    console.log("productTypeGetList action is called");
     return{
         type: PRODUCT_TYPE_GET_LIST
     }
