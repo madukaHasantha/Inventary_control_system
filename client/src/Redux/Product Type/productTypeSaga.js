cf0b61a9e0a45d53d86ec20c70a738a761d551ff
madukaHasantha <77095852+madukaHasantha@users.noreply.github.com>
2024-02-05 13:28:25 +0530

    dev: some changes in UI

--- client/src/Redux/Product Type/productTypeSaga.js
+++ client/src/Redux/Product Type/productTypeSaga.js
@@ -5,7 +5,7 @@ import {PRODUCT_TYPE_GET_LIST, SET_PRODUCT_TYPE_GET_LIST} from "../constatnt";
 
 function* getProductType() {
     try {
-      console.log("getProductType saga is calle");
+      console.log("getProductType saga is called");
       let response = yield fetch(
         "http://localhost:4000/ICMS/productsTypeRouts/get_all_products_types"
       );
